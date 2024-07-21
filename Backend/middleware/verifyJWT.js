ac66cead5f81014ba50b4636c34cae36a99fe6da
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-07-21 19:47:44 +0530

    Update verifyJWT.js

--- Backend/middleware/verifyJWT.js
+++ Backend/middleware/verifyJWT.js
@@ -20,30 +20,3 @@ const verifyJWT = (req, res, next) => {
 }
 module.exports = verifyJWT
 
-/*const jwt=require('jsonwebtoken');
-require('dotenv').config();
-
-
-const verifyJWT= async(req ,res,next)=>
-{
-  const authHeader = req.headers['authorization'];
-  if(!authHeader) res.sendStatus(401);
-    console.log(authHeader);
-
-    const token =authHeader.split(' ')[1];
-    jwt.verify(
-        token ,
-        process.env.ACCESS_TOKEN_SECRET ,
-        (err ,decoded) =>
-        {
-            if(err) return res.sendStatus(403); ///invalid token
-
-            req.user = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next(); 
-        }
-    )
-}
-
-module.exports = verifyJWT ;  
- */
