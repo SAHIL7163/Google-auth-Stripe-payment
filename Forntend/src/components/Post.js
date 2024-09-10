940e8e727d25bc9d28f9380dee3e01cfdc584660
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-09-10 19:04:17 +0530

    Update Post.js

--- Forntend/src/components/Post.js
+++ Forntend/src/components/Post.js
@@ -22,7 +22,7 @@ const Post = ({post,postIndex}) => {
     <div className='post-image-1 d-flex flex-column justify-content-center'>
       <div>
           {(postIndex % 2 !== 0  ||  width <=768 )? (
-            <img className="img-fluid" src={`http://localhost:3500/${post.imageUrl}`} alt='' />
+            <img className="img-fluid" src={`https://blogsite-3gsy.onrender.com/${post.imageUrl}`} alt='' />
           ) : null}
         </div>
     </div>
